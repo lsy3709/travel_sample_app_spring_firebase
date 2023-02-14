235f279b69ec5ad0bf6661a70efe8c5bad24de6a
lsy3709 <lsy3709@naver.com>
2023-02-14 11:37:00 +0900

    230214 동영상 선택 후, 일기 작성, 상세화면에서 프리퍼런스에 저장된 파일의 uri 를 읽어서 출력 확인 완료.

--- app/src/main/java/com/android4/travel/DiaryFiles/Base64Util.kt
+++ app/src/main/java/com/android4/travel/DiaryFiles/Base64Util.kt
@@ -2,8 +2,12 @@ package com.android4.travel.DiaryFiles
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Environment
 import android.util.Base64
+import android.util.Log
 import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Base64Util {
 
@@ -20,6 +24,38 @@ class Base64Util {
          fun stringToBitMap(base64: String?): Bitmap {
              val encodeByte = Base64.decode(base64,Base64.DEFAULT)
              return BitmapFactory.decodeByteArray(encodeByte,0,encodeByte.size)
+         }
+
+         fun mp4ToBase64(filePath: String):String? {
+             var inputStream: FileInputStream? = null
+             return try {
+                 inputStream = FileInputStream(filePath)
+                 val videoBytes = ByteArray(inputStream.available())
+                 inputStream.read(videoBytes)
+                 Base64.encodeToString(videoBytes, Base64.DEFAULT)
+             } catch (e: Exception) {
+                 null
+             } finally {
+                 try {
+                     inputStream?.close()
+                 } catch (e: Exception) { }
+             }
+         }
+
+         fun Base64Tomp4(base64Encoded: String,filePath: String):Boolean {
+             var outputStream: FileOutputStream? = null
+             return try {
+                 val videoBytes = Base64.decode(base64Encoded, Base64.DEFAULT)
+                 outputStream = FileOutputStream(filePath)
+                 outputStream.write(videoBytes)
+                 true
+             } catch (e: Exception) {
+                 false
+             } finally {
+                 try {
+                     outputStream?.close()
+                 } catch (e: Exception) { }
+             }
          }
 
 
